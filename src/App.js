e03df6a3681730123e3a121a2cb5bd5c3644e335
occludedarp <cmrnmr@gmail.com>
2020-01-23 18:46:40 -0500

    created folders and added creatstore to index

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+
+const mapStateToProps = state => {
+  return{
+    
+  }
+}
+
+export default connect(mapStateToProps, { })(App);
---
+++ src/reducers/reducer.js
@@ -0,0 +1,12 @@
+const initialState = (state = initialState, action) => {
+  switch () {
+    case 'SOMETHING':
+      return {
+        ...state,
+        titleOnProps:
+
+      }
+    default:
+      return state;
+  }
+}
